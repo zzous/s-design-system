2b5ba1d7c4d5a0d0fed5764aed522d45463ed4b3
yjgim <yjgim@strato.co.kr>
2024-09-24 10:12:59 +0900

    버전 추가 및 일부 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  server: {
-    port: 8080
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    base: `${process.env.VITE_BASE_PUBLIC_URL}/`,
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    server: {
+      port: 8080
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      // 빌드 시 console log 제거
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: mode !== 'dev',
+          drop_debugger: mode !== 'dev',
+        },
+      },
     }
-  }
-})
+  })
+}
