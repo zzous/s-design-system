681e771f555bbd99ef01758205be1e2324817e8a
yjgim <yjgim@strato.co.kr>
2024-10-10 13:21:56 +0900

    form 컴포넌트 추가

---
+++ src/components/_common/form/FormTableComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <form class="s-table__form">
+    <slot name="default" />
+  </form>
+</template>
+
+<script setup>
+/**
+ * TODO validate 로직 추가
+ */
+</script>
--- src/components/_common/global-components.js
+++ src/components/_common/global-components.js
@@ -6,6 +6,7 @@ import CustomImagesComponent from './images/CustomImagesComponent.vue'
 import CustomDataTableComponent from './table/CustomDataTableComponent.vue'
 import ViewHeaderComponent from './ViewHeaderComponent.vue'
 import FormItemComponent from './form/FormItemComponent.vue'
+import FormTableComponent from './form/FormTableComponent.vue'
 import CustomModalComponent from './modal/CustomModalComponent.vue'
 import CustomConfirmComponent from './modal/CustomConfirmComponent.vue'
 
@@ -19,6 +20,7 @@ export default function setComponents(app) {
   app.component('SDataTable', CustomDataTableComponent)
   app.component('SSubHeader', ViewHeaderComponent)
   app.component('SFormItem', FormItemComponent)
+  app.component('SFormTable', FormTableComponent)
   app.component('SConfirm', CustomConfirmComponent)
   app.component('SModal', CustomModalComponent)
 
