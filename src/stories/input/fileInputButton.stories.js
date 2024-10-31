1a7024df6f7386a0858ed4322b398e10edf9ce5d
macjjuni <macjjuni@strato.co.kr>
2024-10-31 16:44:17 +0900

    feature. FileInputButton 추가(+스토리북)

---
+++ src/stories/input/fileInputButton.stories.js
@@ -0,0 +1,15 @@
+import {FileInputButton} from '@';
+
+export default {
+    title: 'Input/FileInputButton',
+    component: FileInputButton,
+    tags: ['autodocs'],
+    argTypes: {
+        // 📌 스토리북 작성 가이드 https://storybook.js.org/docs/essentials/controls#annotation
+    },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default = {
+    args: {},
+};
