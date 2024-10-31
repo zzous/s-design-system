c9cd5f292fb3d4890b40d54380c440339ef40225
macjjuni <macjjuni@strato.co.kr>
2024-10-31 16:44:38 +0900

    feature. ScriptEditor 추가(+스토리북)

---
+++ src/stories/editor/scriptEditor.stories.js
@@ -0,0 +1,16 @@
+import { ScriptEditor } from '@';
+
+export default {
+    title: 'Editor/ScriptEditor',
+    component: ScriptEditor,
+    tags: ['autodocs'],
+    argTypes: {
+        // 📌 스토리북 작성 가이드 https://storybook.js.org/docs/essentials/controls#annotation
+    },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default = {
+    args: {
+    },
+};
