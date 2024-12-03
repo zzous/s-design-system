1205344065a6c9f9c463146ef217e252ac1abb7d
yjgim <yjgim@strato.co.kr>
2024-12-03 11:00:37 +0900

    네비 오류 수정
    - 1Depth 스타일 오류
    - 프로젝트가 없는 Navi의 2Depth 클릭 오류 수정

--- src/components/layout/SNavi.vue
+++ src/components/layout/SNavi.vue
@@ -66,7 +66,7 @@
           >
             <template #title>
               <RouterLink
-                v-if="selectedProject?.projectId > 0"
+                v-if="selectedProject?.projectId > 0 || !useProject"
                 class="s-navi-inner-menu-title"
                 :to="subMenu.menuUrl"
               >
@@ -97,6 +97,10 @@ const onClickMenuItem = () => {
 }
 
 const props = defineProps({
+  useProject: {
+    type: Boolean,
+    default: false,
+  },
   projectList: {
     type: Array,
     default: () => [],
@@ -176,144 +180,5 @@ watch(
 </script>
 
 <style scoped lang="scss">
-#s-menu-title-wrapper {
-  color: #ffffff;
-  margin-top: 20px;
-  padding-right: 10px;
-  height: 52px;
-  line-height: 50px;
-}
-#s-menu-title-wrapper a {
-  text-decoration: none;
-}
-
-#s-menu-title {
-  padding-left: 20px;
-  background: $main-color;
-  border-radius: 0 10px 10px 0;
-  font-size: 20px;
-  font-weight: 700;
-  color: #fff;
-  cursor: pointer;
-}
-
-#s-menu-wrapper {
-  width: 300px;
-  padding-right: 30px;
-  padding-left: 30px;
-
-  :deep(.v-list) {
-    padding: 0;
-    .v-list-item-title {
-      @include set-text(300, 18, $s-default--gray-9);
-    }
-    .v-list-group__items {
-      .v-list-item-title {
-        @include set-text(300, 16, $s-default--gray-9);
-      }
-    }
-
-    .v-list-item {
-      .v-list-item__overlay {
-        opacity: 0;
-      }
-    }
-    .v-list-item .v-list-item__prepend .v-list-item__spacer {
-      width: 0;
-    }
-
-    .v-list-group {
-      margin-bottom: 10px;
-
-      .v-list-item--density-default:not(.v-list-item--nav).v-list-group__header {
-        --v-hover-opacity: 0;
-        --v-activated-opacity: 0;
-        --v-focus-opacity: 0;
-        --v-theme-overlay-multiplier: 0;
-
-        min-height: 64px;
-        padding-inline: 0;
-        padding-top: 24px;
-        padding-bottom: 16px;
-        margin: 0;
-
-        .v-list-item__content .v-list-item-title {
-          font-weight: 300;
-          font-size: 18px;
-        }
-      }
-
-      .v-list-group__items .v-list-item {
-        --v-theme-overlay-multiplier: 0;
-        padding-inline: 5px !important;
-        margin: 0;
-        min-height: 40px;
-        font-weight: 300;
-
-        &:hover .v-list-item__overlay {
-          opacity: 0.1;
-          border-radius: 100px !important;
-          background-color: $s-btn--blue-1;
-        }
-
-        &:has(.router-link-active) {
-          .router-link-active {
-            color: $active-font-color;
-            font-weight: 700;
-          }
-        }
-      }
-    }
-  }
-}
-
-// Side 영역
-#project_select:deep(.v-select__selection-text) {
-  @include set-text(400, 14, null);
-}
-
-#project_select_wrapper {
-  width: 300px;
-  height: 88px;
-
-  .project_select {
-    padding: 24px 31px;
-  }
-  &::after {
-    content: '';
-    display: block;
-    width: 270px;
-    height: 0;
-    margin: 0 15px;
-    border-top: 1px solid $s-default--gray-5;
-    top: -1px;
-    position: relative;
-  }
-}
-
-#s-navi {
-  position: fixed;
-  top: 68px;
-  height: calc(100% - 68px);
-  border-right: 1px solid $s-default--gray-5;
-  background-color: white;
-}
-
-.s-navi-inner-menu {
-  padding-inline: 15px !important;
-}
-.s-navi-inner-menu.disabled .s-navi-inner-menu-title {
-  //pointer-events: none;
-  color: grey;
-  cursor: not-allowed;
-}
-
-.s-navi-inner-menu-title {
-  font-weight: 300;
-  text-decoration: none;
-  color: black;
-}
-.s-navi-inner-menu.active:hover .s-navi-inner-menu-title {
-  color: $active-font-color;
-}
+@import url(./SNavi.scss);
 </style>
