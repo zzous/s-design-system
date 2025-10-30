466da4e85899db939140f7294651511f1f7fc517
yeonju kim <yjgim@strato.co.kr>
2025-10-30 15:59:53 +0900

    DetailTooltip 위치 조정 오류 수정

--- src/components/tooltip/SDetailTooltip.vue
+++ src/components/tooltip/SDetailTooltip.vue
@@ -5,9 +5,10 @@
         <v-icon v-if="props.label === 'Label'" icon="mdi-dots-vertical" />
         <span v-else>{{ props.label }}</span>
         <v-overlay
-          absolute
           v-model="isOpen"
           activator="parent"
+          location="bottom"
+          origin="auto"
           scrim
           location-strategy="connected"
           scroll-strategy="reposition"
@@ -47,7 +48,7 @@
   </section>
 </template>
 <script setup>
-import { ref, computed, watch, nextTick, onMounted, onUnmounted } from 'vue'
+import { ref, computed, watch } from 'vue'
 import SSimpleTooltip from './SSimpleTooltip.vue'
 
 const uuid = new Date().getTime().toString()
@@ -66,63 +67,10 @@ const isOpen = ref(false)
 
 const toggle = () => {
   isOpen.value = !isOpen.value
-
-  if (isOpen.value) {
-    // 다음 tick에서 DOM이 업데이트된 후 위치 조정
-    nextTick(() => {
-      adjustTooltipPosition()
-    })
-  }
-}
-
-const adjustTooltipPosition = () => {
-  const tooltipContent = document.querySelector('.s-detail-tooltip__content')
-  if (!tooltipContent) return
-
-  const rect = tooltipContent.getBoundingClientRect()
-  const viewportWidth = window.innerWidth
-  const viewportHeight = window.innerHeight
-
-  let transformX = 0
-  let transformY = 0
-
-  // 오른쪽으로 넘어가는 경우
-  if (rect.right > viewportWidth - 20) {
-    transformX = viewportWidth - rect.right - 20
-  }
-
-  // 왼쪽으로 넘어가는 경우
-  if (rect.left < 20) {
-    transformX = 20 - rect.left
-  }
-
-  // 아래쪽으로 넘어가는 경우
-  if (rect.bottom > viewportHeight - 20) {
-    transformY = viewportHeight - rect.bottom - 20
-  }
-
-  tooltipContent.style.transform = `translate(${transformX}px, ${transformY}px)`
 }
 
 watch(isOpen, (nv, ov) => {
   if (nv && !props.list?.length) isOpen.value = ov
-})
-
-// 윈도우 리사이즈 시 툴팁 위치 재조정
-const handleResize = () => {
-  if (isOpen.value) {
-    adjustTooltipPosition()
-  }
-}
-
-// 컴포넌트 마운트 시 리사이즈 이벤트 리스너 추가
-onMounted(() => {
-  window.addEventListener('resize', handleResize)
-})
-
-// 컴포넌트 언마운트 시 이벤트 리스너 제거
-onUnmounted(() => {
-  window.removeEventListener('resize', handleResize)
 })
 
 const displayText = computed(() => {
@@ -146,18 +94,13 @@ const displayText = computed(() => {
 }
 
 .s-detail-tooltip__content {
-  position: absolute;
-  top: calc(100% + 8px);
-  left: -80px;
   display: flex;
   flex-direction: column;
   width: auto;
-  max-width: calc(100vw - 20px);
+  max-width: 500px;
   border: 1px solid #7c7c7c;
   background: #fff;
   padding: 10px 22px 10px 10px;
-  z-index: 99999;
-  transform-origin: left top;
 
   .detail-item {
     max-width: 360px;
